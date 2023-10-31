53b56c49b93a8d8ecc9f49f029df570bf81223c6
rubenrocha21 <124410608+rubenrocha21@users.noreply.github.com>
2023-10-30 19:37:09 -0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+<?php include 'menu.php' ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-12"><br><br>
+                <center><h1>Sistema de gestión de control escolar</h1>
+                <h3>CETis 107</h3>
+                <img src="logoDGETI.png" alt="" width="30%" class="img-fluid"></center>
+            </div>
+        </div>
+    </div><br>
+    <footer class="text-center"><hr>
+        <span>CETis107 Web App &copy; 2023</span>
+    </footer>
+    <script src="js/bootstrap.js"></script>
+</body>
+</html>
---
+++ inicio.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Catalogo de alumnos</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="jquery-3.7.1.js"></script>
+</head>
+<body>
+
+    <?php include 'menu.php';?>
+
+    <div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h2>
+                Bienvenido al sistema de gestiond de alumnos
+            </h2>
+            <img src="imagenes/logocetis.png" alt="">
+        </div>
+    </div>
+    </div>
+    
+    <footer class="text-center">
+        <hr>
+        CETIS 107 &copy; 2023
+    </footer>
+
+    <script src="js/bootstrap.js"></script>
+</body>
+</html>
